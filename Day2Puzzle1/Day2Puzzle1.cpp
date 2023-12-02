c18c01c7deddcd0157a639d2bd9903c7e44f6505
Avijeet <am388488@gmail.com>
2023-12-02 13:43:23 +0530

    Day 2

---
+++ Day2Puzzle1/Day2Puzzle1.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+auto notPossible = [](const auto& _color) {
+	static std::unordered_map<std::string, int> maxCubes{ {"red", 12}, {"green", 13}, {"blue", 14} };
+	std::string_view color(_color);
+	int count{};
+	auto [ptr, err] = std::from_chars(color.data(), color.data() + color.size(), count);
+	return count > maxCubes[std::string(ptr + 1, color.data() + color.size())];
+};
+
+int main() {
+	std::fstream input("input.txt");
+	int sum = 0, id = 0;
+	std::string line;
+	while (std::getline(input, line)) {
+		++id;
+		for (const auto& game : line | std::views::split(std::string(": ")) | std::views::drop(1)) {
+			bool validGame = true;
+			for (const auto& set : game | std::views::split(std::string("; "))) {
+				auto color = set | std::views::split(std::string(", "));
+				if (std::ranges::any_of(color, notPossible)) {
+					validGame = false;
+					break;
+				}
+			}
+			if (validGame) {
+				sum += id;
+			}
+		}
+	}
+	std::cout << sum << '\n';
+}
+
---
+++ Day2Puzzle2/Day2Puzzle2.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+int main() {
+	std::fstream input("input.txt");
+	int sum = 0;
+	std::unordered_map<std::string, int> maxCubes;
+	auto calculateMax = [&maxCubes](const auto& _cubes) {
+		std::string_view cubes(_cubes);
+		int count;
+		auto [ptr, err] = std::from_chars(cubes.data(), cubes.data() + cubes.size(), count);
+		std::string color(ptr + 1, cubes.data() + cubes.size());
+		maxCubes[color] = std::max(maxCubes[color], count);
+	};
+	std::string line;
+	while (std::getline(input, line)) {
+		for (const auto& game : line | std::views::split(std::string(": ")) | std::views::drop(1)) {
+			for (const auto& set : game | std::views::split(std::string("; "))) {
+				auto colors = set | std::views::split(std::string(", "));
+				std::ranges::for_each(colors, calculateMax);
+			}
+			sum += maxCubes["red"] * maxCubes["green"] * maxCubes["blue"];
+			maxCubes.clear();
+		}
+	}
+	std::cout << sum << '\n';
+}
+
