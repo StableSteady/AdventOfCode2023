fd22e2624adc513a9eb87fdb8270869b1d6334cd
Avijeet <am388488@gmail.com>
2023-12-04 15:36:27 +0530

    Day 4 Puzzle 1

---
+++ Day4Puzzle1/Day4Puzzle1.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+auto notEmpty = [](const auto& s) {
+	return !std::string_view(s).empty();
+	};
+
+auto toInt = [](const auto& s) {
+	std::string_view num(s);
+	int val{};
+	std::from_chars(s.data(), s.data() + s.size(), val);
+	return val;
+	};
+
+int main() {
+	std::ifstream input("input.txt");
+	std::string line;
+	int sum = 0;
+	while (std::getline(input, line)) {
+		auto cards = (line | std::views::split(std::string(": ")) | std::views::drop(1)).front() | std::views::split(std::string(" | ")) | std::ranges::to<std::vector>();
+		std::unordered_map<int, bool> isWinning;
+		for (int num : cards.front() | std::views::split(std::string(" ")) | std::views::filter(notEmpty) | std::views::transform(toInt)) {
+			isWinning[num] = true;
+		}
+		int points = 0;
+		for (int num : cards.back() | std::views::split(std::string(" ")) | std::views::filter(notEmpty) | std::views::transform(toInt)) {
+			if (isWinning[num]) {
+				points <<= 1;
+				points = std::max(points, 1);
+			}
+		}
+		sum += points;
+	}
+	std::cout << sum << '\n';
+}
